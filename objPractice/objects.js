2a1c461c08248a4ab0371c910913c7752dd4c427
Jose Correa <correajc1799@gmail.com>
2021-11-18 20:59:13 -0800

    Reorganized js code

--- objPractice/objects.js
+++ objPractice/objects.js
@@ -3,16 +3,12 @@ let myLibrary = [
    
 ];
 
-
 function Book(title, author, pages, read){
     this.title = title; //string
     this.author = author; //string
     this.pages = pages; //int
     this.read = read;   //boolean
 }
-
-
-
 
 function addBookToLibrary(objArr){
     for(let i = 0; i < objArr.length; i++){
@@ -25,6 +21,7 @@ function modifyStats(){
     //get elements
     const booksRead = document.getElementById("bookStats");
     const pagesRead = document.getElementById("pagesReadStats");
+
     let counter = myLibrary.length;
     for(let i = 0;i < myLibrary.length;i++){
         if(!myLibrary[i].read){
@@ -36,6 +33,7 @@ function modifyStats(){
     for(let i = 0; i < myLibrary.length;i++){
         sum += parseInt(myLibrary[i].pages);
     }
+
     let totalBooksRead = counter;
     booksRead.innerHTML = totalBooksRead;
     pagesRead.innerHTML = sum;
@@ -45,61 +43,51 @@ function modifyStats(){
 
 document.getElementById("btn").addEventListener("click", function(){
     const card = document.createElement("div");
+    const cardHeaderInput = document.createElement("Label");
+    const bookTitleInput = document.createElement("INPUT")
+    const p1Label = document.createElement("Label");
+    const p1Input = document.createElement("INPUT")
+    const p2Label = document.createElement("Label");
+    const p2Input = document.createElement("INPUT")
+    const readingCheck = document.createElement("div");
+    const checkMarkDiv = document.createElement("div");
+    const p3Label = document.createElement("Label");
+    const p3Input = document.createElement("INPUT");
+    const p4Label = document.createElement("Label");
+    const p4Input = document.createElement("INPUT");
+    const submit = document.createElement("button");
+    const btnContent = document.createTextNode("SUBMIT");
+    const latestElement = document.getElementById("bookDisplay");
+
     card.classList.add("smallCardTemp");
     card.setAttribute("id", "tempCard");
-    // make div to put header in
-    // make header
-    const cardHeaderInput = document.createElement("Label");
+    checkMarkDiv.classList.add("radioBtns")
+    submit.setAttribute("id", "subBtn");
+
     cardHeaderInput.innerHTML = "Enter Book Title: "
     cardHeaderInput.htmlFor = "bookTitleInput"
-
-    var bookTitleInput = document.createElement("INPUT")
     bookTitleInput.id = "bookTitleInput"
     bookTitleInput.name = "bookTitleInput"
     bookTitleInput.type = "text";
-    //put header within div
-
-    const p1Label = document.createElement("Label");
     p1Label.innerHTML = "Enter Book Author: "
     p1Label.htmlFor = "p1Input"
-
-    var p1Input = document.createElement("INPUT")
     p1Input.id = "p1Input"
     p1Input.name = "p1Input"
     p1Input.type = "text";
-    
-
-    const p2Label = document.createElement("Label");
     p2Label.innerHTML = "Enter Page Amount: "
     p2Label.htmlFor = "p2Input"
-
-    var p2Input = document.createElement("INPUT")
     p2Input.id = "p2Input"
     p2Input.name = "p2Input"
     p2Input.type = "text";
-
-    const readingCheck = document.createElement("div");
     readingCheck.innerHTML = "Have you read this book?"
-
-
-    const checkMarkDiv = document.createElement("div");
-    checkMarkDiv.classList.add("radioBtns")
-
-    const p3Label = document.createElement("Label");
     p3Label.innerHTML = "Yes"
     p3Label.htmlFor = "p3Input"
-
-    const p3Input = document.createElement("INPUT");
     p3Input.id = "yes";
     p3Input.name = "p3Input"
     p3Input.type = "radio";
     p3Input.value = "yes"
-
-    const p4Label = document.createElement("Label");
     p4Label.innerHTML = "No"
     p4Label.htmlFor = "p4Input"
-
-    const p4Input = document.createElement("INPUT");
     p4Input.id = "no";
     p4Input.name = "p4Input"
     p4Input.type = "radio";
@@ -109,14 +97,7 @@ document.getElementById("btn").addEventListener("click", function(){
     checkMarkDiv.appendChild(p3Input)
     checkMarkDiv.appendChild(p4Label)
     checkMarkDiv.appendChild(p4Input)
-
-    const submit = document.createElement("button");
-    submit.setAttribute("id", "subBtn");
-    const btnContent = document.createTextNode("SUBMIT");
     submit.appendChild(btnContent);
-
-
-
     card.appendChild(cardHeaderInput);
     card.appendChild(bookTitleInput);
     card.appendChild(p1Label);
@@ -126,15 +107,12 @@ document.getElementById("btn").addEventListener("click", function(){
     card.appendChild(readingCheck)
     card.appendChild(checkMarkDiv)
     card.appendChild(submit)
-
-    //append card to main content
-    const latestElement = document.getElementById("bookDisplay");
     latestElement.appendChild(card);
 });
 
+//passes info to array upon submission and creates a new card, updates stats
 document.addEventListener('click',function(e){
     if(e.target && e.target.id== 'subBtn'){
-          //do something
           var check = false;
           if(document.getElementById('yes').checked) {
             //Yes radio button is checked
@@ -151,14 +129,13 @@ document.addEventListener('click',function(e){
         }
 
         myLibrary.push(temp);
-        addBookToLibrary(myLibrary);
         deleteTemp();
         createCard();
         modifyStats();
-
      }
  });
 
+ //remove card from display and book info from array
  document.addEventListener('click',function(e){
     if(e.target && e.target.id== 'removeButton'){
           //do something
@@ -166,13 +143,11 @@ document.addEventListener('click',function(e){
           var index = parentDiv.getAttribute('data-index')
           myLibrary.splice(index-1, 1);
           parentDiv.remove();
-          console.log("------")
-          addBookToLibrary(myLibrary);
           modifyStats();
      }
  });
 
-
+ //changes color of read div, updates array
  document.addEventListener('click', function(e){
     if(e.target && e.target.id== 'notReadP'){
         var div = e.target;
@@ -196,51 +171,46 @@ document.addEventListener('click',function(e){
      }
  })
 
-
-
-
-
  function deleteTemp(){
      var tempCard = document.getElementById("tempCard");
      tempCard.remove();
  }
 
+ function readCheck(){
+    const bookCheck = "Not Read"
+    if(myLibrary[i].read){
+        bookCheck = "Read";
+    }
+    return bookCheck;
+ }
+
  function displayCards(){
     for(let i = 0; i < myLibrary.length; i++){
         const newCard = document.createElement("div");
-        newCard.classList.add("smallCard");
-        newCard.setAttribute("id", "newCard");
-        // make div to put header in
         const newCardDiv = document.createElement("div");
-        newCardDiv.classList.add("smallCardTitle");
-        // make header
         const newCardHeader = document.createElement("h3");
         const newCardHeaderText = document.createTextNode(myLibrary[i].title);
-        newCardHeader.appendChild(newCardHeaderText);
-        //put header within div
-
-        newCardDiv.appendChild(newCardHeader);
-
         const p1 = document.createElement("p");
-        p1Text = document.createTextNode(myLibrary[i].author);
-        p1.appendChild(p1Text);
-
+        const p1Text = document.createTextNode(myLibrary[i].author);
         const p2 = document.createElement("p");
-        p2Text = document.createTextNode(myLibrary[i].pages);
-        p2.appendChild(p2Text);
-
-        var bookCheck = "Not Read"
+        const p2Text = document.createTextNode(myLibrary[i].pages);
+        var bookCheck = readCheck();
         const p3 = document.createElement("p");
-        if(myLibrary[i].read){
-            bookCheck = "Read";
-        }
         const p3Content = document.createTextNode(bookCheck);
-        p3.appendChild(p3Content);
-
         const removeButton = document.createElement("button");
-        buttonText = document.createTextNode("Remove");
-        removeButton.appendChild(buttonText)
+        const buttonText = document.createTextNode("Remove");
+
+        newCard.classList.add("smallCard");
+        newCard.setAttribute("id", "newCard");
+        newCardDiv.classList.add("smallCardTitle");
         removeButton.setAttribute("id", "removeButton")
+
+        newCardHeader.appendChild(newCardHeaderText);
+        newCardDiv.appendChild(newCardHeader);
+        p1.appendChild(p1Text);
+        p2.appendChild(p2Text);
+        p3.appendChild(p3Content);
+        removeButton.appendChild(buttonText)
 
         newCard.appendChild(newCardDiv);
         newCard.appendChild(p1)
@@ -255,65 +225,59 @@ document.addEventListener('click',function(e){
     }
  }
 
- //displaying the user inputted card, new
- function createCard(){
-    const newCard = document.createElement("div");
-    newCard.classList.add("smallCard");
-    newCard.setAttribute("id", "newCard");
-    // make div to put header in
-    const newCardDiv = document.createElement("div");
-    newCardDiv.classList.add("smallCardTitle");
-    // make header
-    const newCardHeader = document.createElement("h3");
-    const newCardHeaderText = document.createTextNode(myLibrary[myLibrary.length-1].title);
-    newCardHeader.appendChild(newCardHeaderText);
-    //put header within div
-
-    newCardDiv.appendChild(newCardHeader);
-
-    const p1 = document.createElement("p");
-    p1Text = document.createTextNode(myLibrary[myLibrary.length-1].author);
-    p1.appendChild(p1Text);
-
-    const p2 = document.createElement("p");
-    p2Text = document.createTextNode(myLibrary[myLibrary.length-1].pages);
-    p2.appendChild(p2Text);
-
-    const p3 = document.createElement("p");
-    p3.setAttribute("id", "readP")
-    var bookCheck;
+ function readCheckArray(){
+    var bookCheck = "Not Read"
     if(myLibrary[myLibrary.length-1].read){
         bookCheck = "Read";
         myLibrary[myLibrary.length-1].read = true;
     }else{
-        bookCheck = "Not Read";
         myLibrary[myLibrary.length-1].read = false;
     }
+    return bookCheck;
+ }
+
+ //displaying the user inputted card, new
+ function createCard(){
+    const newCard = document.createElement("div");
+    const newCardDiv = document.createElement("div");
+    const newCardHeader = document.createElement("h3");
+    const newCardHeaderText = document.createTextNode(myLibrary[myLibrary.length-1].title);
+    const p1 = document.createElement("p");
+    const p1Text = document.createTextNode(myLibrary[myLibrary.length-1].author);
+    const p2 = document.createElement("p");
+    const p2Text = document.createTextNode(myLibrary[myLibrary.length-1].pages);
+    const p3 = document.createElement("p");
+    var bookCheck = readCheckArray();
     const p3Content = document.createTextNode(bookCheck);
-    p3.appendChild(p3Content);
-
     const removeButton = document.createElement("button");
-    buttonText = document.createTextNode("Remove");
-    removeButton.appendChild(buttonText)
+    const buttonText = document.createTextNode("Remove");
+
+    newCard.classList.add("smallCard");
+    newCard.setAttribute("id", "newCard");
+    newCardDiv.classList.add("smallCardTitle");
+    p3.setAttribute("id", "readP")
     removeButton.setAttribute("id", "removeButton")
+    newCard.setAttribute("data-index", myLibrary.length)
 
-
+    newCardHeader.appendChild(newCardHeaderText);
+    newCardDiv.appendChild(newCardHeader);
+    p1.appendChild(p1Text);
+    p2.appendChild(p2Text);
+    p3.appendChild(p3Content);
+    removeButton.appendChild(buttonText)
     newCard.appendChild(newCardDiv);
     newCard.appendChild(p1)
     newCard.appendChild(p2)
     newCard.appendChild(p3)
     newCard.appendChild(removeButton)
 
-    newCard.setAttribute("data-index", myLibrary.length)
-
-
     //append card to main content
     const latestElement = document.getElementById("bookDisplay");
     latestElement.appendChild(newCard);
  }
 
-
 addBookToLibrary(myLibrary);
+
 if(typeof myLibrary !== 'undefined'){
     displayCards();
     console.log(myLibrary.length)
